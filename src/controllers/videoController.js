6f616ad53e5dc6a903f6eb12dba8a0dc385a583a
haleyjung <73789849+haleyjung@users.noreply.github.com>
2021-10-13 16:52:41 -0400

    debugged param

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,8 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send("See Video");
+export const see = (req, res) => {
+	console.log(req.params);
+	return res.send("Watch");
+};
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
