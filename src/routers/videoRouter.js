88be6dc68dd08312269d421ffa90e0624d47ef7f
haleyjung <73789849+haleyjung@users.noreply.github.com>
2021-10-17 17:29:47 -0400

    added post request

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { see, edit, deleteVideo, upload } from "../controllers/videoController";
+import {
+	watch,
+	deleteVideo,
+	upload,
+	getEdit,
+	postEdit,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.get("/:id(\\d+)/edit", getEdit);
+videoRouter.post("/:id(\\d+)/edit", postEdit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/upload", upload);
 
 export default videoRouter;
