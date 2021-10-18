2c2114cb3551fdaa87ac72a50a375e2d9d2eaf02
haleyjung <73789849+haleyjung@users.noreply.github.com>
2021-10-18 10:34:36 -0400

    setup redirect

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -41,7 +41,11 @@ export const getEdit = (req, res) => {
 	return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
 };
 
-export const postEdit = (req, res) => {};
+export const postEdit = (req, res) => {
+	const { id } = req.params;
+	return res.redirect(`/videos/${id}`);
+};
+
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
